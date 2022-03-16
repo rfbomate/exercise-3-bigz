f0718b3edfb44804aee3d24608fd20991500335b
gerencia@datalabcenter.com <gerencia@datalabcenter.com>
2022-03-15 21:45:01 -0500

    first commit

---
+++ app.ts
@@ -0,0 +1,26 @@
+import { BigZ } from './utilities/BigZ';
+
+console.clear();
+try {
+    const inputMatriz = [
+        [1, 3, 4, 10],
+        [2, 5, 9, 11],
+        [6, 8, 12, 15],
+        [7, 13, 14, 16]
+    ];
+    const bigZInstance: BigZ = new BigZ(inputMatriz);
+
+    const result: number[] = bigZInstance.drawTour();
+
+    console.log(result);
+} catch (e) {
+    console.error('\x1b[31m', e);
+}
+
+
+
+
+
+
+
+
---
+++ utilities/BigZ.ts
@@ -0,0 +1,18 @@
+
+class BigZ {
+    _matriz: number[][]
+
+    constructor(pMatriz: number[][]) {
+        this._matriz = pMatriz;
+    }
+
+    drawTour(): number[] {
+        return [0]
+    }
+
+}
+
+
+
+
+export { BigZ }
