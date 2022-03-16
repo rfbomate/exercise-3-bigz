57684675bfcc7deaeb3189505160b7b16f583322
gerencia@datalabcenter.com <gerencia@datalabcenter.com>
2022-03-16 01:43:38 -0500

    Write better comments

--- app.ts
+++ app.ts
@@ -1,20 +1,27 @@
 import { BigZ } from './utilities/BigZ';
 
-console.clear();
+console.clear(); //Clean the console
+
+//when we call external functions different exceptions can occur, for that I implemented try/catch structure
 try {
+    //define input matrix
     const inputMatrix = [
         [1, 3, 4, 10],
         [2, 5, 9, 11],
         [6, 8, 12, 15],
         [7, 13, 14, 16]
-
     ];
+
+    //create an instance of the class
     const bigZInstance: BigZ = new BigZ(inputMatrix);
 
+    // get result of the exercise 
     const result: number[] = bigZInstance.drawTour();
 
+    //print in console the result
     console.log(result);
 } catch (e) {
+    //if an exception occurs, I print to the console an error with red color
     console.error('\x1b[31m', e);
 }
 
--- utilities/BigZ.ts
+++ utilities/BigZ.ts
@@ -1,32 +1,41 @@
-
+// i created a class for model the business logic of the exrcise
 class BigZ {
+    //private attribute of the class. Represents the input array when the object is instantiated
     _matrix: number[][]
 
+    //class constructor. It is used to initialize the attributes
     constructor(pMatrix: number[][]) {
-
+        //verify that all rows has the same numbers of items
         let isCorrectMatrix = pMatrix.every((item) => item.length == pMatrix[0].length);
-        if (!isCorrectMatrix) {
+        if (!isCorrectMatrix) { // if the array is not formatted correctly, it throws an error
             throw "The rows of the matrix must have the same numbers of items";
         }
+        //if the matrix has a correct format, I inicialize the attribute
         this._matrix = pMatrix;
     }
 
+    //main method. Answer the exercise question. Return the tour following the bigZ method theory
     drawTour(): number[] {
 
         const resultListLength: number = this.calculateLengthResultList();
         let resultList: number[] = [];
 
-        let state: string = "Initial"; //Initial state
-        let rowIndex: number = 0;
-        let columnIndex: number = 0;
+        let state: string = "Initial"; //real time state
+        let rowIndex: number = 0; //real time row index
+        let columnIndex: number = 0; //real time column index
 
+        //I define the last index to vist
         const finalIndex: number[] = [this._matrix.length - 1, this._matrix[this._matrix.length - 1].length - 1];
 
-
+        //while I haven't visited all the elements
         while (resultList.length < resultListLength) {
+
+            //if I found the last index, I break the loop
             if (finalIndex[0] == rowIndex && finalIndex[1] == columnIndex) {
                 break;
             }
+
+            //I check the different states that the algorithm can take
             switch (state) {
                 case 'Initial': { //initial 
                     resultList.push(this._matrix[rowIndex][columnIndex]);
@@ -83,8 +92,9 @@ class BigZ {
         return resultList;
     }
 
+    //return the size of the result list
     private calculateLengthResultList(): number {
-        let result = 0;
+        let result: number = 0;
 
         this._matrix.forEach((item) => {
             result += item.length;
@@ -93,9 +103,10 @@ class BigZ {
         return result;
     }
 
+    //I get an array with the items that I found in this tour
     private makeUpRightDiagonal(position: number[]): number[][] {
 
-        let [initialRow, initialColumn] = position;
+        let [initialRow, initialColumn]: number[] = position;
         let result: number[][] = [[], [position[0], position[1]]];
         if (this._matrix[initialRow][initialColumn] == undefined) {
             return result;
@@ -116,6 +127,7 @@ class BigZ {
         return result;
     }
 
+    //I get an array with the items that I found in this tour
     private makeLeftDownDiagonal(position: number[]): number[][] {
 
         let [initialRow, initialColumn] = position;
@@ -147,5 +159,5 @@ class BigZ {
 
 
 
-
+//I export the class
 export { BigZ }
