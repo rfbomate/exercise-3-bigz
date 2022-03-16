5958f19f2538597fb212877ffb83354967f38153
gerencia@datalabcenter.com <gerencia@datalabcenter.com>
2022-03-16 00:56:55 -0500

    Adjusting readme

--- app.ts
+++ app.ts
@@ -2,15 +2,14 @@ import { BigZ } from './utilities/BigZ';
 
 console.clear();
 try {
-    const inputMatriz = [
-        [1, 2, 3, 4, 9],
-        [5, 6, 7, 8],
-        [9, 10, 11, 12],
-        [13, 14, 15, 16],
-        [17, 18, 19, 20]
+    const inputMatrix = [
+        [1, 3, 4, 10],
+        [2, 5, 9, 11],
+        [6, 8, 12, 15],
+        [7, 13, 14, 16]
 
     ];
-    const bigZInstance: BigZ = new BigZ(inputMatriz);
+    const bigZInstance: BigZ = new BigZ(inputMatrix);
 
     const result: number[] = bigZInstance.drawTour();
 
